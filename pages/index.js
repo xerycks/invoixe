2f67a56cc1b8d1276b4cf0824a24836578558ace
xerycks <xerycks@gmail.com>
2021-07-05 22:27:52 +0530

    login form changed

--- pages/index.js
+++ pages/index.js
@@ -36,31 +36,32 @@ export default function Home() {
             <i className="bi bi-file-text"></i>&nbsp;&nbsp;Invoices with the <span className="font-bolder text-tertiary">X-factor !</span>
             </brand>    
             <h2 className="mt-6">Get Into Your Invoices</h2>
-            <form className="login-form mt-6 w-100">
-                <div className="mb-5 input-group-md text-xs">
+            <form className="login-form mt-6 w-100 row h-auto">
+                <div className="mb-3 input-group-md text-xs col-sm-12 col-md-6">
                     <label className="form-label" htmlFor="email">Email address</label>
                     <input type="email" className="form-control" id="email" placeholder="Your email address" />
                 </div>
-                <div className="mb-5">
-                    <label className="form-label" htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Password" />
+                <div className="mb-3 input-group-md text-xs col-sm-12 col-md-6">
+                    <label className="form-label" >Password</label>
+                    <input type="password" className="form-control" placeholder="Password" />
                 </div>
-                <div className="mb-5">
+                <div className="mb-5 text-xs">
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" name="check_example" id="check_example" />
-                        <label className="form-check-label" htmlFor="check_example">
+                        <label className="form-check-label mt-n1 text-xs" htmlFor="check_example">
                             Keep me logged in
                         </label>
                     </div>
                 </div>
                 <div>
-                    <a href="/dashboard" className="btn btn-primary w-full w-md-50 mx-1">Login</a>
+                    <a href="/dashboard" className="btn btn-primary w-full me-5 w-md-max mb-4 my-md-auto">Login</a>
+                    <a href="#" className="btn btn-neutral w-full w-md-max font-bold"><i className="bi bi-google"></i>&nbsp;&nbsp;SignIn With Google</a>
                 </div>
             </form>
 
             <hr className="w-70 mx-auto  mt-10"/>
 
-            <a href="/signup" className="text-dark"><p className="text-sm float-end">Don&apos;t have a account ? <span className="text-primary">Register here !</span></p></a>
+            <a href="/signup" className="text-dark btn-outline-secondary"><p className="text-sm float-end">Don&apos;t have a account ? <span className="text-primary">Register here !</span></p></a>
 
             </div>
         </div>
